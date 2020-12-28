f15c1cdcc376a9035856d36505af6124e88aa636
Purusottam Mohapatra <35234210+purusottam11@users.noreply.github.com>
2020-12-28 13:22:15 +0530

    best time to buy and sell stock

--- Purusottam.java
+++ Purusottam.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+public class Purusottam {
     public int[] twoSum(int[] nums, int target) {
         int arr[] = new int[2];
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -15,6 +15,23 @@ public class TwoSum {
         }
         return arr;
     }
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        if (prices == null || prices.length == 0) {
+            return profit;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i] - min);
+            }
+        }
+        return profit;
+    }
+
+
 
     public static void main(String[] args) {
 
